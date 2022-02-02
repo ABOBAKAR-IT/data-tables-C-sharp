2acbef41e3e6b13ed6157c897d72bf3919be9a64
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-02-02 13:56:46 +0500

    create 3 input box get value from text box and show in table

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,34 +29,78 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.TextBox();
+            this.roll = new System.Windows.Forms.TextBox();
+            this.cnic = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(33, 32);
+            this.dataGridView1.Location = new System.Drawing.Point(71, 203);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(646, 170);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(626, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(71, 77);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 20);
+            this.name.TabIndex = 2;
+            // 
+            // roll
+            // 
+            this.roll.Location = new System.Drawing.Point(241, 74);
+            this.roll.Name = "roll";
+            this.roll.Size = new System.Drawing.Size(100, 20);
+            this.roll.TabIndex = 3;
+            // 
+            // cnic
+            // 
+            this.cnic.Location = new System.Drawing.Point(424, 74);
+            this.cnic.Name = "cnic";
+            this.cnic.Size = new System.Drawing.Size(100, 20);
+            this.cnic.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cnic);
+            this.Controls.Add(this.roll);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox roll;
+        private System.Windows.Forms.TextBox cnic;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,24 +12,14 @@ namespace data_tables
 {
     public partial class Form1 : Form
     {
+        DataTable data = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
-            DataTable data = new DataTable();
-            data.Columns.Add("Name");
-            data.Columns.Add("Roll");
-            data.Columns.Add("CNIC");
-
-            DataRow row1 = data.NewRow();
-
-            row1["Name"] = "Ali";
-            row1["Roll"] = "22";
-            row1["CNIC"] = "24134513451345";
-            data.Rows.Add(row1);
 
 
             dataGridView1.DataSource = data;
-            dataGridView1.Refresh();
         }
 
         private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -39,6 +29,24 @@ namespace data_tables
 
         private void DataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
         {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            data.Columns.Add("Name");
+            data.Columns.Add("Roll");
+            data.Columns.Add("CNIC");
+
+            DataRow row1 = data.NewRow();
+
+            row1["Name"] = name.Text;
+            row1["Roll"] = roll.Text;
+            row1["CNIC"] = cnic.Text;
+            data.Rows.Add(row1);
+            dataGridView1.Refresh();
 
         }
     }
