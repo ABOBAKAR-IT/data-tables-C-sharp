4e4dbe17814d4c1d8b4e0c9f055c761239fe7334
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-02-14 08:32:04 +0500

    file handling write read and edit done

--- Form1.cs
+++ Form1.cs
@@ -7,21 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace data_tables
 {
     public partial class Form1 : Form
     {
-        DataTable data = new DataTable();
-
+        DataTable data = new DataTable();//data = table
+        string path = @"C:\Users\RANA ABOBAKAR\Desktop\table_data.txt";
         public Form1()
         {
             InitializeComponent();
-
             data.Columns.Add("Name");
             data.Columns.Add("Roll");
             data.Columns.Add("CNIC");
             dataGridView1.DataSource = data;
+            readfile();
         }
 
        
@@ -39,16 +39,16 @@ namespace data_tables
 
             DataRow row1 = data.NewRow();
 
-            row1["Name"] = name.Text;
+            row1["Name"] = name.Text;//textBox1.Text
             row1["Roll"] = roll.Text;
             row1["CNIC"] = cnic.Text;
             data.Rows.Add(row1);
             dataGridView1.Refresh();
+            write_file();
 
             name.Text = "";
             roll.Text = "";
             cnic.Text = "";
-
 
         }
 
@@ -62,6 +62,7 @@ namespace data_tables
                     {
                         row["Name"] = name.Text;
                         row["CNIC"] = cnic.Text;
+                        edit_file();
                         break;
                     }
                 }
@@ -71,6 +72,7 @@ namespace data_tables
             {
                 MessageBox.Show("Kindly Fill the Requirements!");
             }
+          
         }
 
         private void Button2_Click(object sender, EventArgs e)
@@ -82,6 +84,7 @@ namespace data_tables
                     if (row["Roll"].ToString() == roll.Text)
                     {
                         data.Rows.Remove(row);
+                        edit_file();
                         break;
                     }
                 }
@@ -92,6 +95,59 @@ namespace data_tables
                 MessageBox.Show("Kindly Fill the Requirements!");
             }
         }
+
+        //Write File
+        public void write_file()
+        {
+            string path = @"C:\Users\RANA ABOBAKAR\Desktop\table_data.txt";
+            FileStream fs = File.Open(path, FileMode.Append,FileAccess.Write);
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                    sw.WriteLine(roll.Text);
+                    sw.WriteLine(name.Text);
+                    sw.WriteLine(cnic.Text);
+            }
+        }
+        private void readfile()
+        {
+            string path = @"C:\Users\RANA ABOBAKAR\Desktop\table_data.txt";
+            if (File.Exists(path))
+            {
+                FileStream fs = File.Open(path, FileMode.Open);
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        DataRow row = data.NewRow();
+                        row["Roll"] = sr.ReadLine();
+                        row["Name"] = sr.ReadLine();
+                        row["CNIC"] = sr.ReadLine();
+                        data.Rows.Add(row);
+                    }
+
+                }
+            }
+            else
+            {
+                FileStream file = new FileStream(path, FileMode.Create);
+                file.Close();
+            }
+        }
+        private void edit_file()
+        {
+            string path = @"C:\Users\RANA ABOBAKAR\Desktop\table_data.txt";
+            if (File.Exists(path)) { File.Delete(path); }
+            FileStream fs = File.Open(path, FileMode.Create);
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                foreach (DataRow row in data.Rows) { 
+                sw.WriteLine(row["Roll"]);
+                sw.WriteLine(row["Name"]);
+                sw.WriteLine(row["CNIC"]);
+            }
+            }
+        }
+
     }
     
 }
